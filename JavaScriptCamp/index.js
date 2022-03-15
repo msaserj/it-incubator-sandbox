e742bba09045285823321e1f02eea260a7f68e7d
msa <serg.ks@gmail.com>
2022-03-15 16:54:57 +0300

    Appending Variables to Strings

--- JavaScriptCamp/index.js
+++ JavaScriptCamp/index.js
@@ -1,3 +1,4 @@
-// Only change code below this line
-const myName = "Serj";
-const myStr = "My name is " + myName + " and I am well!";
+// Change code below this line
+const someAdjective = "cool";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
