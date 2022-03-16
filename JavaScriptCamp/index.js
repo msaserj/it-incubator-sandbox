ac1831af222fae980d18e071be2366e8985721b4
msa <serg.ks@gmail.com>
2022-03-16 10:34:54 +0300

    Passing Values to Functions with Arguments

--- JavaScriptCamp/index.js
+++ JavaScriptCamp/index.js
@@ -1,4 +1,5 @@
-function reusableFunction() {
-   console.log("Hi World");
+function functionWithArgs(param1, param2) {
+   console.log(param1 + param2);
 }
-reusableFunction()
+
+functionWithArgs(2, 3);
