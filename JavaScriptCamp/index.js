f3f3c9c5b7e3f29fd3192ead7b54b29a27724f9d
msa <serg.ks@gmail.com>
2022-03-17 15:18:54 +0300

    Accessing Nested Objects

--- JavaScriptCamp/index.js
+++ JavaScriptCamp/index.js
@@ -1,25 +1,13 @@
-const myMusic = [
-   {
-      "artist": "Billy Joel",
-      "title": "Piano Man",
-      "release_year": 1973,
-      "formats": [
-         "CD",
-         "8T",
-         "LP"
-      ],
-      "gold": true
-   },
-   {
-      "artist": "Orbital",
-      "title": "Halcion",
-      "release_year": 1992,
-      "formats": [
-         "CD",
-         "Cassette",
-         "LP"
-      ],
-      "gold": true
+const myStorage = {
+   "car": {
+      "inside": {
+         "glove box": "maps",
+         "passenger seat": "crumbs"
+      },
+      "outside": {
+         "trunk": "jack"
+      }
    }
-];
+};
 
+const gloveBoxContents = myStorage.car.inside["glove box"];
