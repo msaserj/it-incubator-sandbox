dd44c50a57fe126c804dcf1188f800b056d446e4
msa <serg.ks@gmail.com>
2022-03-17 16:04:48 +0300

    Iterate with JavaScript Do...While Loops

--- JavaScriptCamp/index.js
+++ JavaScriptCamp/index.js
@@ -1,11 +1,9 @@
 // Setup
-const myArr = [2, 3, 4, 5, 6];
+const myArray = [];
+let i = 10;
 
 // Only change code below this line
-let total = 0;
-for (let i = myArr.length - 1; i >= 0; i--) {
-   total += myArr[i];
-}
-
-console.log(total)
-console.log(myArr.length)
+do {
+   myArray.push(i);
+   i++;
+} while (i < 5);
