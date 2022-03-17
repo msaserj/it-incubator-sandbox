b5f26019cf70938f7a51cdb15a5cc99d22af3056
msa <serg.ks@gmail.com>
2022-03-17 16:38:56 +0300

    Generate Random Fractions with JavaScript

--- JavaScriptCamp/index.js
+++ JavaScriptCamp/index.js
@@ -1,9 +1,8 @@
-function sum(arr, n) {
+function randomFraction() {
+
    // Only change code below this line
-   if (n <= 0) {
-      return 0;
-   } else {
-      return sum(arr, n - 1) + arr[n - 1];
-   }
-}
+
+   return Math.random();
+
    // Only change code above this line
+}
