18dc213f55015cb8233e12b44af10572af4f5217
msaserj <serg.ks@gmail.com>
2022-03-19 12:25:38 +0300

    Prevent Object Mutation

--- JavaScriptCamp/ES6.js
+++ JavaScriptCamp/ES6.js
@@ -1,12 +1,16 @@
-const s = [5, 7, 2];
-function editInPlace() {
+function freezeObj() {
+   const MATH_CONSTANTS = {
+      PI: 3.14
+   };
    // Only change code below this line
-
-
-   s[0] = 2;
-   s[1] = 5;
-   s[2] = 7;
+   Object.freeze(MATH_CONSTANTS);
 
    // Only change code above this line
+   try {
+      MATH_CONSTANTS.PI = 99;
+   } catch (ex) {
+      console.log(ex);
+   }
+   return MATH_CONSTANTS.PI;
 }
-editInPlace();
+const PI = freezeObj();
