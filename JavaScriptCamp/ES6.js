cf7666c0e423a070f8b173010f5ac3761b13d184
msaserj <serg.ks@gmail.com>
2022-03-19 12:20:12 +0300

    Mutate an Array Declared with const

--- JavaScriptCamp/ES6.js
+++ JavaScriptCamp/ES6.js
@@ -1,9 +1,12 @@
-function checkScope() {
-   let i = 'function scope';
-   if (true) {
-      let i = 'block scope';
-      console.log('Block scope i is: ', i);
-   }
-   console.log('Function scope i is: ', i);
-   return i;
+const s = [5, 7, 2];
+function editInPlace() {
+   // Only change code below this line
+
+
+   s[0] = 2;
+   s[1] = 5;
+   s[2] = 7;
+
+   // Only change code above this line
 }
+editInPlace();
